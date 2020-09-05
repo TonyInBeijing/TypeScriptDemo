393cd3564cffd88f225b3cc239bd90e9f4ccccee
TonyInBeijing <1300565735@qq.com>
2020-09-05 23:03:08 +0800

    用户接口修改

--- ts-demo03-express02/routes/users.ts
+++ ts-demo03-express02/routes/users.ts
@@ -3,7 +3,7 @@ const router = express.Router();
 
 /* GET users listing. */
 router.get('/', (req: Request, res: Response, next: NextFunction): void => {
-    res.send('respond with a resource');
+    res.send('这是用户接口');
 });
 
 export default router
