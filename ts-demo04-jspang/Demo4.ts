57dbd7269b1ee4e05690024f7226eeb48f3723f3
TonyInBeijing <1300565735@qq.com>
2021-04-14 22:28:17 +0800

    函数参数定义和返回类型定义

---
+++ ts-demo04-jspang/Demo4.ts
@@ -0,0 +1,28 @@
+/**
+ * @description 函数参数定义和返回类型定义
+ */
+function getTotal1(a: number, b: number): number {
+    return a + b;
+}
+const res: number = getTotal1(1, 2);
+
+function sayHello(): void {
+    console.log('hello!');
+}
+
+function errorHandler(): never {
+    throw new Error();
+    console.log('hello');
+}
+
+function add({ a, b }: { a: number, b: number }) {
+    return a + b;
+}
+
+const total1 = add({ a: 1, b: 2 });
+
+function getNumber({ one }: { one: number }) {
+    return one;
+}
+
+const one = getNumber({ one: 1 });
