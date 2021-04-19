e17ba280699880a6d46367593cc22bc66701f5ef
TonyInBeijing <1300565735@qq.com>
2021-04-19 19:48:52 +0800

    interface 接口

--- ts-demo04-jspang/Demo7.ts
+++ ts-demo04-jspang/Demo7.ts
@@ -7,14 +7,37 @@ interface Person {
     age: number;
     height: number;
     weight?: number;
+    [propname: string]: any;
+    say(): string;
+}
+
+interface Teacher extends Person {
+    teach(): string;
 }
 
 const person1 = {
     name: 'Tony',
     age: 18,
     height: 176,
-    weight: 80
+    weight: 80,
+    sex: '男',
+    say() {
+        return 'hello';
+    },
+    teach() {
+        return 'teach';
+    }
 };
+
+class PersonClass implements Person {
+    name: 'Jack';
+    age: 18;
+    height: 180;
+    weight: 90;
+    say() {
+        return 'hi';
+    }
+}
 
 const screenResume = (person1: Person) => {
     const { name, age, height } = person1;
@@ -22,12 +45,14 @@ const screenResume = (person1: Person) => {
     age >= 24 || height <= 160 && console.log(name + '你被淘汰');
 };
 
-const getResume = (person1: Person) => {
+const getResume = (person1: Teacher) => {
     const { name, age, height } = person1;
     console.log('name:' + name);
     console.log('age:' + age);
     console.log('height:' + height);
     person1.weight && console.log('体重是:' + person1.weight);
+    person1.sex && console.log('性别是:', person1.sex);
+    person1.say && console.log(person1.say());
 }
 screenResume(person1);
 getResume(person1);
