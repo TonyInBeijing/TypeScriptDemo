18c70e853fdac4212fdd3348c8ce7b58d1d25074
TonyInBeijing <1300565735@qq.com>
2021-04-19 23:11:24 +0800

    constructor构造函数

---
+++ ts-demo04-jspang/Demo9.ts
@@ -0,0 +1,24 @@
+/*
+ * @Author: Tony 
+ * @Date: 2021-04-19 23:05:22 
+ * @Last Modified by: Tony
+ * @Last Modified time: 2021-04-19 23:09:46
+ * @description constructor 构造函数
+ */
+
+class Person {
+    constructor(public name: string) {
+        this.name = name;
+    }
+}
+
+class Teacher extends Person {
+    constructor(public age: number) {
+        super('Tony');
+    }
+}
+
+const person = new Person('Tony');
+const teacher = new Teacher(18);
+console.log(person.name);
+console.log(teacher);
