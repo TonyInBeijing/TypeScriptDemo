a8947c432f288faf6f1c469d83915e3ae4ee5088
TonyInBeijing <1300565735@qq.com>
2021-04-21 22:05:35 +0800

    class

--- ts-demo04-jspang/Demo9.ts
+++ ts-demo04-jspang/Demo9.ts
@@ -2,7 +2,7 @@
  * @Author: Tony 
  * @Date: 2021-04-19 23:05:22 
  * @Last Modified by: Tony
- * @Last Modified time: 2021-04-19 23:09:46
+ * @Last Modified time: 2021-04-21 22:05:30
  * @description constructor 构造函数
  */
 
@@ -17,7 +17,6 @@ class Teacher extends Person {
         super('Tony');
     }
 }
-
 const person = new Person('Tony');
 const teacher = new Teacher(18);
 console.log(person.name);
